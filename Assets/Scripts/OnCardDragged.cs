110459887e4f7a72af09a30024126fbbc94b0abc
Seongjin <43747041+Sungjin01@users.noreply.github.com>
2019-12-25 23:13:57 +0900

    commit

--- Assets/Scripts/OnCardDragged.cs
+++ Assets/Scripts/OnCardDragged.cs
@@ -7,7 +7,10 @@ using UnityEngine.EventSystems;
 public class OnCardDragged : MonoBehaviour
 {
     Vector3 position;
-    bool isDragging;
+    bool isDragging, isFinish;
+    public bool isFocusing;
+    public SceneManager sceneManager;
+    public GameObject leftText, rightText;
 
     void Start(){
         EventTrigger eventTrigger = gameObject.AddComponent<EventTrigger>();
@@ -21,6 +24,7 @@ public class OnCardDragged : MonoBehaviour
         eventTrigger.triggers.Add(entry_Up);
         
         position = this.GetComponent<RectTransform>().localPosition;
+        isFinish = false;
     }
 
     void Update(){
@@ -34,19 +38,75 @@ public class OnCardDragged : MonoBehaviour
             if(-rect.offsetMax.y > 600 && y > 0)y = 0;
 
             transform.position += new Vector3(x * -0.05f, y * -0.01f, 0);
-            Debug.Log(transform.position.x);
+            //Debug.Log(transform.position.x);
 
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, (float)((440 - transform.position.x)*0.022) + 2.2f));
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, (float)((550 - transform.position.x)*0.022)));
+
+            if(transform.position.x > 700) rightText.SetActive(true);
+            else rightText.SetActive(false);
+
+            if(transform.position.x < 400) leftText.SetActive(true);
+            else leftText.SetActive(false);
         }
     }
 
     void OnDown(PointerEventData data){
-        isDragging = true;
+        if(!isFinish && isFocusing)isDragging = true;
     }
 
     void OnUp(PointerEventData data){
-        isDragging = false;
-        transform.localPosition = position;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        rightText.SetActive(false);
+        leftText.SetActive(false);
+        if(transform.position.x > 800)
+        {
+            isFinish = true;
+            isDragging = false;
+            StartCoroutine(Finish(true));
+        }
+        if(transform.position.x < 300)
+        {
+            isFinish = true;
+            isDragging = false;
+            StartCoroutine(Finish(false));
+        }
+        if(!isFinish){
+            isDragging = false;
+            transform.localPosition = position;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+    }
+
+    IEnumerator Finish(bool isGoRight){
+        if(isGoRight)
+        {
+            while(transform.position.x < 2000){
+                transform.position += new Vector3(2000 * Time.deltaTime , -1000 * Time.deltaTime, 0);
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, (float)((440 - transform.position.x)*0.022) + 2.2f));
+                yield return new WaitForEndOfFrame();
+            }
+            sceneManager.SetUI(false); 
+            transform.localPosition = position;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            isFinish = false;
+        }
+        else
+        {
+            while(transform.position.x > -1000){
+                transform.position -= new Vector3(2000 * Time.deltaTime , 1000 * Time.deltaTime, 0);
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, (float)((440 - transform.position.x)*0.022) + 2.2f));
+                yield return new WaitForEndOfFrame();
+            }
+            sceneManager.SetUI(true); 
+            transform.localPosition = position;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            isFinish = false;
+        }
+    }
+
+    public void SetFocus(bool focus){
+        isFocusing = focus;
+    }
+    public bool GetFocus(){
+        return isFocusing;
     }
 }
--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -2,41 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class SceneManager : MonoBehaviour
 {
     List<int> premises = new List<int>();
      //어떤 상황에 게임을 끝낼지에 대한 대전제 ex)hp 자원이 0이 되면 게임오버.
     List<Branch> branches = new List<Branch>();
-    int currentBranchId;
+    public int currentBranchId;
     public GameObject CardPrefab;
-    public Image card;
-    public Text content, cardText;
+    public GameObject card1, card2, card3;
+    public Text content;
+    public Sprite defaultSprite;
 
     void Awake()//개발자 디버그용 브랜치 생성
     {
-        branches.Add(new Branch(1,
-        "게임이 시작되었습니다. 시작하시겠습니까?",
-        "images.png",
+        Branch defaultBranch = new Branch(0, "기본 브랜치 입니다.", "",
+        new Destination(1, 0, 0, ""),
+        new Destination(1, 0, 0, ""));
+        
+        Branch branch = new Branch(1, "게임 시작, 승리하시겠습니까?", "",
         new Destination(2, 0, 0, "예"),
-        new Destination(3, 0, 0, "아니요")));
+        new Destination(3, 0, 0, "아니오"));
 
-        branches.Add(new Branch(2,
-        "승리하였습니다. 재시작하시겠습니까?",
-        null,
-        new Destination(1, 0, 0, "예"),
-        new Destination(0, 0, 0, "아니요")));
+        Branch branch1 = new Branch(2, "승리하셨습니다. 다시하시겠습니까?", "",
+        new Destination(1, 0, 0, "yes"),
+        new Destination(0, 0, 0, "no"));
 
-        branches.Add(new Branch(3,
-        "패배하였습니다. 재시작하시겠습니까?",
-        null,
-        new Destination(1, 0, 0, "예"),
-        new Destination(0, 0, 0, "아니요")));
+        Branch branch2 = new Branch(3, "패배하셨습니다. 다시하시겠습니까?", "",
+        new Destination(1, 0, 0, "Yeah!!!!"),
+        new Destination(0, 0, 0, "Never!!!!"));
+
+        branches.Add(defaultBranch);
+        branches.Add(branch);
+        branches.Add(branch1);
+        branches.Add(branch2);
     }
     void Start(){
-        currentBranchId = 1; // 1: 시작 브랜치 아이디
+        currentBranchId = 0; // 0으로 시작
+        SetUI(true);
     }
 
+    //json 파싱
+    IEnumerator ServerInit(string uri)
+    {
+        using(UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if(webRequest.isNetworkError)
+            {
+                Debug.Log("Networking Error : " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log("Networking Ok : "+webRequest.downloadHandler.text);
+            }
+        }
+    }
     Branch GetBranch(int branchId){
         foreach (Branch i in branches)
         {
@@ -48,16 +71,154 @@ public class SceneManager : MonoBehaviour
         return null;
     }
 
-    void SetUI(){
+    public void SetUI(bool isLeft){
         Branch branch = GetBranch(currentBranchId);
+        if(isLeft)
+        {
+            currentBranchId = branch.destinFirst.branchId;
+        }
+        else
+        {
+            currentBranchId = branch.destinSecond.branchId;
+        }
+        branch = GetBranch(currentBranchId);
+        
 
-        GameObject card = GameObject.Instantiate(CardPrefab);
-        if(branch.img == null){
-            cardText = card.GetComponentInChildren<Text>();
-            cardText.text = branch.text;
+        OnCardDragged cardDragged1 = card1.GetComponent<OnCardDragged>();
+        OnCardDragged cardDragged2 = card2.GetComponent<OnCardDragged>();
+        OnCardDragged cardDragged3 = card3.GetComponent<OnCardDragged>();
+        
+        
+        if(cardDragged1.GetFocus()){
+            //card1이 바깥으로 밀어졌을 때
             
-        }else{
+            card1.GetComponentInChildren<Text>().text = "";
 
+            card1.transform.SetAsFirstSibling();
+
+            if(branch.img.Equals("")){
+                content.text = "";
+                StartCoroutine(TurnAndChangeImage(2, branch, true));
+            }else{
+                content.text = branch.text;
+                StartCoroutine(TurnAndChangeImage(2, branch, false));
+                //이미지 set
+            }
+            cardDragged2.leftText.GetComponentInChildren<Text>().text = branch.destinFirst.text;
+            cardDragged2.rightText.GetComponentInChildren<Text>().text = branch.destinSecond.text;
+        }
+        else if(cardDragged2.GetFocus())
+        {
+            //card2이 바깥으로 밀어졌을 때    
+            card2.GetComponentInChildren<Text>().text = "";
+            card2.transform.SetAsFirstSibling();
+
+            if(branch.img.Equals("")){
+                content.text = "";
+                StartCoroutine(TurnAndChangeImage(3, branch, true));
+            }else{
+                content.text = branch.text;
+                StartCoroutine(TurnAndChangeImage(3, branch, false));
+                //이미지 set
+            }
+            cardDragged3.leftText.GetComponentInChildren<Text>().text = branch.destinFirst.text;
+            cardDragged3.rightText.GetComponentInChildren<Text>().text = branch.destinSecond.text;
+        }else
+        {
+            //card3이 바깥으로 밀어졌을 때
+            
+            card3.GetComponentInChildren<Text>().text = "";
+
+            card3.transform.SetAsFirstSibling();
+
+            if(branch.img.Equals("")){
+                content.text = "";
+                StartCoroutine(TurnAndChangeImage(1, branch, true));
+            }else{
+                content.text = branch.text;
+                StartCoroutine(TurnAndChangeImage(1, branch, false));
+                //이미지 set
+            }
+            cardDragged1.leftText.GetComponentInChildren<Text>().text = branch.destinFirst.text;
+            cardDragged1.rightText.GetComponentInChildren<Text>().text = branch.destinSecond.text;
+        }
+    }
+
+    IEnumerator TurnAndChangeImage(int cardCode, Branch branch, bool isDefaultSprite){
+        switch(cardCode){
+            case 1:
+                card1.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                while(card1.transform.rotation.y < 0.7){
+                    card1.transform.Rotate(new Vector3(0, 3f, 0));
+                    yield return new WaitForEndOfFrame();
+                }
+                //이미지 변경, 텍스트 입력 
+                if(isDefaultSprite)
+                {
+                    card1.GetComponentInChildren<Text>().text = branch.text;
+                    card1.GetComponent<Image>().sprite = defaultSprite;
+                }else
+                {
+                    //이미지 변경, 이미지가 있을 때
+                }
+
+                while(card1.transform.rotation.y > 0){
+                    card1.transform.Rotate(new Vector3(0, -3f, 0));
+                    yield return new WaitForEndOfFrame();
+                }
+                card1.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                card1.GetComponent<OnCardDragged>().SetFocus(true);
+                card2.GetComponent<OnCardDragged>().SetFocus(false);
+                card3.GetComponent<OnCardDragged>().SetFocus(false);
+                break;
+            case 2:
+                while(card2.transform.rotation.y < 0.7){
+                    card2.transform.Rotate(new Vector3(0, 3f, 0));
+                    yield return new WaitForEndOfFrame();
+                }
+                //이미지 변경, 텍스트 입력 
+                if(isDefaultSprite)
+                {
+                    card2.GetComponentInChildren<Text>().text = branch.text;
+                    card2.GetComponent<Image>().sprite = defaultSprite;
+                }else
+                {
+                    //이미지 변경, 이미지가 있을 때
+                }
+
+                while(card2.transform.rotation.y > 0){
+                    card2.transform.Rotate(new Vector3(0, -3f, 0));
+                    yield return new WaitForEndOfFrame();
+                }
+                card2.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                card1.GetComponent<OnCardDragged>().SetFocus(false);
+                card2.GetComponent<OnCardDragged>().SetFocus(true);
+                card3.GetComponent<OnCardDragged>().SetFocus(false);
+                break;
+            case 3:
+                while(card3.transform.rotation.y < 0.7){
+                    card3.transform.Rotate(new Vector3(0, 3f, 0));
+                    yield return new WaitForEndOfFrame();
+                }
+                //이미지 변경, 텍스트 입력 
+                if(isDefaultSprite)
+                {
+                    card3.GetComponentInChildren<Text>().text = branch.text;
+                    card3.GetComponent<Image>().sprite = defaultSprite;
+                }else
+                {
+                    //이미지 변경, 이미지가 있을 때
+                }
+
+                while(card3.transform.rotation.y > 0){
+                    card3.transform.Rotate(new Vector3(0, -3f, 0));
+                    yield return new WaitForEndOfFrame();
+                }
+                card3.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                card1.GetComponent<OnCardDragged>().SetFocus(false);
+                card2.GetComponent<OnCardDragged>().SetFocus(false);
+                card3.GetComponent<OnCardDragged>().SetFocus(true);
+                break;
         }
     }
 }
